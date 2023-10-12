cc6a6576ac6489d2c0feb6fe9e74c42cc520e652
Rian Perassoli <rian.perassoli@gmail.com>
2023-10-12 20:27:46 -0300

    created components for history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,56 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
-  return <h1>history</h1>
+  return (
+    <HistoryContainer>
+      <h1>My history</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa 01</td>
+              <td>20 minutes</td>
+              <td>Ha cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 01</td>
+              <td>20 minutes</td>
+              <td>Ha cerca de 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 01</td>
+              <td>20 minutes</td>
+              <td>Ha cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 01</td>
+              <td>20 minutes</td>
+              <td>Ha cerca de 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
